e58363e681fabcfbcea7393340ad34058f44819f
Alan Ensina <alanvinicius.ce@gmail.com>
2023-06-10 16:11:50 +0100

    Deleting order service, it will be restarted

--- order-service/src/main/java/alanensina/orderservice/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package alanensina.orderservice.repositories;
-
-import alanensina.orderservice.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
